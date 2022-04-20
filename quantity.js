e7749f08382898e4312dd7180944322b56b3e04a
Jure Mraović <mraovic.jure@gmail.com>
2022-04-20 12:54:40 +0200

    Update quantity.js

--- quantity.js
+++ quantity.js
@@ -1,11 +1,9 @@
 $('.qty-plus').on('click',function() {
-    // Vars
     let thisQtyInput = $(this).parent().find('.qty');
     let thisMax = parseInt(thisQtyInput.attr('max'));
     let thisQtyVal = thisQtyInput.val();
     let thisQtyIncreased = parseInt(thisQtyVal) + 1;
 
-    // If there's a limit (if thisMax is a number) increasing until the limit is reached, else increasing infinite
     if($.isNumeric(thisMax) == true) {
         if (thisQtyIncreased <= thisMax) {
             thisQtyInput.val(thisQtyIncreased);
@@ -16,7 +14,6 @@ $('.qty-plus').on('click',function() {
 });
 
 $('.qty-minus').on('click', function() {
-    // Vars
     let thisQtyInput = $(this).parent().find('.qty');
     let thisQtyVal = thisQtyInput.val();
     let thisQtyDecreased = parseInt(thisQtyVal) - 1;
