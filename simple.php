3eb19cbb066c331ecb737e0c0596e633d51af2f8
Jure Mraović <mraovic.jure@gmail.com>
2022-04-20 12:42:04 +0200

    Update simple.php

--- simple.php
+++ simple.php
@@ -1,12 +1,14 @@
 
-/**  
-
-In woocommerce/add-to-cart/simple.php template wrapping default quantity input field (PHP part below) with .quantity-wrapper div and adding span elements for minus/plus icons */
+<!-- 
+In woocommerce/add-to-cart/simple.php template wrapping default quantity input field (PHP part below) with .quantity-wrapper div and adding span elements for minus/plus icons 
+-->
 
 <div class="quantity-wrapper">
     <span class="qty-icon qty-minus"><i class="fal fa-minus"></i></span>
+    
     <?php
         do_action( 'woocommerce_before_add_to_cart_quantity' );
+    
         woocommerce_quantity_input(
             array(
                 'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
@@ -14,8 +16,10 @@ In woocommerce/add-to-cart/simple.php template wrapping default quantity input f
                 'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
             )
         );
+    
         do_action( 'woocommerce_after_add_to_cart_quantity' );
     ?>
+    
     <span class="qty-icon qty-plus"><i class="fal fa-plus"></i></span>
 </div>
 
